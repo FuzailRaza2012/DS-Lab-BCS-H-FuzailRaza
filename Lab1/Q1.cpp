91e12e256b7661b26b9e509be7d060198803d075
FuzailRaza2012 <fuzailraza11205@gmail.com>
2025-08-24 21:12:41 +0500

    Update Q1.cpp

--- Lab1/Q1.cpp
+++ Lab1/Q1.cpp
@@ -18,14 +18,18 @@ class BankAccount{
 		
 		BankAccount(BankAccount &obj){
 			balance = obj.balance;
-			balance -= 200;
 		}	
 };
 
 int main(){
 	BankAccount account1;
+	
+	cout << "Printing Balance of Account 1: " << account1.balance << endl;
+	
 	BankAccount account2(1000);
 	BankAccount account3 = account2;
+	
+	account3.balance -= 200;
 	
 	cout << "Printing Balance of Account" << endl << "2: " << account2.balance << endl << "3: " << account3.balance << endl << "2: " << account2.balance << endl;
 	
