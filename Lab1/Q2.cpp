39f0737d1fc2f3cc023f18c2bc4ac09b62961210
FuzailRaza2012 <fuzailraza11205@gmail.com>
2025-08-24 21:16:44 +0500

    Update Q2.cpp

--- Lab1/Q2.cpp
+++ Lab1/Q2.cpp
@@ -1,61 +1,69 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-
 using namespace std;
 
 class Exam{
-	public:
-		char* studentName;
-		char* examDate;
-		int score;
-		
-		Exam(){
-			studentName = NULL;
-			examDate = NULL;
-			score = 0;
-		}
-		
-		Exam(const char* n, const char* d, int s){
-			studentName = new char[strlen(n) + 1];
-			strcpy(studentName, n);
-			
-			examDate = new char[strlen(d) + 1];
-			strcpy(examDate, d);
-			
-			score = s;
-		}
-		
-		void setDeets(const char* n, const char* d, int s){
-			if (studentName == NULL || examDate == NULL){
-				studentName = new char[strlen(n) + 1];
-				strcpy(studentName, n);
-			
-				examDate = new char[strlen(d) + 1];
-				strcpy(examDate, d);
-			
-				score = s;
-				
-				cout << "Details Inserted" << endl;
-			}
-			else{
-				cout << "Details already Exist" << endl;
-			}
-		}
-		
-		void ShowDeets(){
-			cout << "Details: " << endl;
-			cout << "Name: " << studentName << endl;
-			cout << "Exam date: " << examDate << endl;
-			cout << "Score: " << score << endl;
-		}
+public:
+    char* studentName;
+    char* examDate;
+    int score;
+
+    Exam(){
+        studentName = NULL;
+        examDate = NULL;
+        score = 0;
+    }
+
+    Exam(const char* n, const char* d, int s){
+        studentName = new char[strlen(n) + 1];
+        strcpy(studentName, n);
+
+        examDate = new char[strlen(d) + 1];
+        strcpy(examDate, d);
+
+        score = s;
+    }
+
+    void setDeets(const char* n, const char* d, int s){
+        if (studentName == NULL || examDate == NULL){
+            studentName = new char[strlen(n) + 1];
+            strcpy(studentName, n);
+
+            examDate = new char[strlen(d) + 1];
+            strcpy(examDate, d);
+
+            score = s;
+        }
+        else{
+            cout << "Details already Exist" << endl;
+        }
+    }
+
+    void ShowDeets(){
+        cout << "Details: " << endl;
+        cout << "Name: " << studentName << endl;
+        cout << "Exam date: " << examDate << endl;
+        cout << "Score: " << score << endl;
+    }
+
+    ~Exam(){
+        delete[] studentName;
+        delete[] examDate;
+    }
 };
 
 int main(){
-	Exam exam2;
-	Exam exam1("Fuzail Raza", "20-12-2024", 67);
-	exam1.ShowDeets();
-	
-	exam2.setDeets("Sudheer Rathore", "27-08-2024", 67);
-	exam2.ShowDeets();
+    Exam exam1("Fuzail Raza", "20-12-2024", 67);
+    exam1.ShowDeets();
+
+    Exam exam2;
+    exam2.setDeets("Sudheer Rathore", "27-08-2024", 85);
+    exam2.ShowDeets();
+
+    Exam exam3 = exam1;
+    cout << "Exam3 (copied from exam1): ";
+    exam3.ShowDeets();
+
+    return 0;
 }
